59d1cb82863a57a13ae2d8f5dd3046ca556695e0
prabalp <prabalpanda2008@gmail.com>
2021-09-11 11:53:19 +0530

    urls

--- apiHub/urls.py
+++ apiHub/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from carApi import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('car', views.add_car),
+    path('<str:car_name>', views.get_car),
 ]
